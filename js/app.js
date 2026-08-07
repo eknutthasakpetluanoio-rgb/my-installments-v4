87672dec8b71eeeb6e5d259b689381ffd35f40ce
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-07 15:48:43 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ async function init() {
     console.log("PayNest v1 Starting...");
 
     const contracts = loadContracts();
-
+console.table(contracts);
     updateSummary(contracts);
 
     renderContracts(contracts);
