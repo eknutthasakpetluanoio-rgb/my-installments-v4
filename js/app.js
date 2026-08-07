0bb59f5acf6fc3c36f63296252fa4be9994ace4a
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-07 15:21:40 +0700

    Create js/app.js

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/* ==========================================
+   PayNest v1
+   File : app.js
+   Version : 1.0.0
+   Description : Application Bootstrap
+========================================== */
+
+import { loadContracts } from "./contracts.js";
+import { updateSummary } from "./summary.js";
+import { renderContracts } from "./ui.js";
+import { registerPWA } from "./pwa.js";
+
+function init() {
+
+    console.log("PayNest v1 Starting...");
+
+    const contracts = loadContracts();
+
+    updateSummary(contracts);
+
+    renderContracts(contracts);
+
+    registerPWA();
+
+    console.log("PayNest Ready");
+
+}
+
+document.addEventListener("DOMContentLoaded", init);
