1d5aeb380c5426f19c63de3cb31b77f3df4d4fe1
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-07 17:20:59 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,122 @@
+/**
+ * ==========================================================
+ * PayNest v1
+ * Service Worker
+ * Version : 1.0.0
+ * Build   : 1
+ * ==========================================================
+ */
+
+'use strict';
+
+const CACHE_NAME = 'paynest-v1.0.0';
+
+const APP_SHELL = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+/* ==========================================================
+   INSTALL
+========================================================== */
+
+self.addEventListener('install', (event) => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME).then((cache) => {
+
+            return cache.addAll(APP_SHELL);
+
+        })
+
+    );
+
+    self.skipWaiting();
+
+});
+
+/* ==========================================================
+   ACTIVATE
+========================================================== */
+
+self.addEventListener('activate', (event) => {
+
+    event.waitUntil(
+
+        caches.keys().then((keys) => {
+
+            return Promise.all(
+
+                keys.map((key) => {
+
+                    if (key !== CACHE_NAME) {
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+    self.clients.claim();
+
+});
+
+/* ==========================================================
+   FETCH
+========================================================== */
+
+self.addEventListener('fetch', (event) => {
+
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then((cachedResponse) => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then((networkResponse) => {
+
+                        if (!networkResponse || networkResponse.status !== 200) {
+                            return networkResponse;
+                        }
+
+                        const responseClone = networkResponse.clone();
+
+                        caches.open(CACHE_NAME)
+                            .then((cache) => {
+
+                                cache.put(event.request, responseClone);
+
+                            });
+
+                        return networkResponse;
+
+                    })
+
+                    .catch(() => {
+
+                        return caches.match('./index.html');
+
+                    });
+
+            })
+
+    );
+
+});
